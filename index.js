8cdd706f1135f477c64145ada575deb57014fa82
drexmose <155102908+drexmose@users.noreply.github.com>
2024-10-30 15:54:14 +0300

    drex mods

--- index.js
+++ index.js
@@ -1,146 +1,216 @@
 require("./database/global");
-const func = require("./database/place");
+const func = require('./database/place');
 const readline = require('readline');
-const question = _0x3e2073 => {
-  const _0x4b85ff = readline.createInterface({
+const question = _0x16ecb7 => {
+  const _0x4ed637 = readline.createInterface({
     'input': process.stdin,
     'output': process.stdout
   });
-  return new Promise(_0x5f204e => {
-    _0x4b85ff.question(_0x3e2073, _0x5f204e);
+  return new Promise(_0xf1f02d => {
+    _0x4ed637.question(_0x16ecb7, _0xf1f02d);
   });
 };
+async function connectToWhatsApp() {
+	const isActive = {
+		status: true
+	}
+}
 async function startSesi() {
-  const _0x41eefd = makeInMemoryStore({
+  const _0x19952b = makeInMemoryStore({
     'logger': pino().child({
-      'level': 'silent',
+      'level': "silent",
       'stream': "store"
     })
   });
   const {
-    state: _0x560611,
-    saveCreds: _0x2e820c
-  } = await useMultiFileAuthState('./session');
+	Creator,
+	Access,
+	Access_Bot
+} = require("./drex/fakoff.js")
+const {
+	smsg,
+	isUrl,
+	generateMessageTag,
+	getBuffer,
+	getSizeMedia,
+	fetchJson,
+	sleep,
+	question
+} = require("./lib/myfunction");
+try {
+	var {
+		Low,
+		JSONFile
+	} = require("lowdb")
+} catch (e) {
+	var {
+		Low,
+		JSONFile
+	} = require("./lib/lowdb")
+}
+
+const yargs = require("yargs/yargs")
   const {
-    version: _0x37dc6a,
-    isLatest: _0x5a59c9
+    state: _0x282728,
+    saveCreds: _0x145ebc
+  } = await useMultiFileAuthState("./session");
+  const {
+    version: _0x467b29,
+    isLatest: _0x221efb
   } = await fetchLatestBaileysVersion();
-  console.log(chalk.green.bold("\n░█████╗░██████╗░██╗░░░██╗██╗░░░██╗\n██╔══██╗██╔══██╗╚██╗░██╔╝╚██╗░██╔╝\n███████║██████╦╝░╚████╔╝░░╚████╔╝░\n██╔══██║██╔══██╗░░╚██╔╝░░░░╚██╔╝░░\n██║░░██║██████╦╝░░░██║░░░░░░██║░░░\n╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░░░░╚═╝░░░\n\nABYY BOTZ BUG V8.5.0"));
-  const _0x1e9935 = {
-    'version': _0x37dc6a,
+  console.log(chalk.green.bold("\n░█████╗░██████╗░██╗░░░██╗██╗░░░██╗\n██╔══██╗██╔══██╗╚██╗░██╔╝╚██╗░██╔╝\n███████║██████╦╝░╚████╔╝░░╚████╔╝░\n██╔══██║██╔══██╗░░╚██╔╝░░░░╚██╔╝░░\n██║░░██║██████╦╝░░░██║░░░░░░██║░░░\n╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░░░░╚═╝░░░\n\nABYY BOTZ BUG V8.0.0"));
+  const _0x6c9e1d = {
+    'version': _0x467b29,
     'keepAliveIntervalMs': 0x7530,
     'printQRInTerminal': false,
     'logger': pino({
       'level': "fatal"
     }),
-    'auth': _0x560611,
-    'browser': ['Ubuntu', "Chrome", "20.0.04"]
+    'auth': _0x282728,
+    'browser': ['Ubuntu', "Chrome", '20.0.04']
   };
-  const _0x4077f6 = func.makeWASocket(_0x1e9935);
-  if (true && !_0x4077f6.authState.creds.registered) {
-    const _0x1c2e3b = await question(chalk.green("\nEnter Your Number\nNumber : "));
-    const _0x192a41 = await _0x4077f6.requestPairingCode(_0x1c2e3b.trim());
-    console.log(chalk.red("YOUR CODE PAIR : " + _0x192a41 + " "));
+  const _0x19e3df = func.makeWASocket(_0x6c9e1d);
+  if (true && !_0x19e3df.authState.creds.registered) {
+    const _0x282549 = await question(chalk.green("\nEnter Your Number\nNumber : "));
+    const _0x53c1e2 = await _0x19e3df.requestPairingCode(_0x282549.trim());
+    console.log(chalk.red("YOUR CODE PAIR : " + _0x53c1e2 + " "));
   }
-  _0x41eefd.bind(_0x4077f6.ev);
-  _0x4077f6.ev.on('connection.update', async _0xfd720b => {
-    const {
-      connection: _0x1ef720,
-      lastDisconnect: _0x2497e3
-    } = _0xfd720b;
-    if (_0x1ef720 === "close") {
-      const _0xd82737 = new Boom(_0x2497e3?.["error"])?.["output"]["statusCode"];
-      console.log(color(_0x2497e3.error, 'deeppink'));
-      if (_0x2497e3.error == "Error: Stream Errored (unknown)") {
-        process.exit();
-      } else {
-        if (_0xd82737 === DisconnectReason.badSession) {
-          console.log(color("Bad Session File, Please Delete Session and Scan Again"));
-          process.exit();
-        } else {
-          if (_0xd82737 === DisconnectReason.connectionClosed) {
-            console.log(color("[SYSTEM]", "white"), color("Connection closed, reconnecting...", "deeppink"));
-            process.exit();
-          } else {
-            if (_0xd82737 === DisconnectReason.connectionLost) {
-              console.log(color("[SYSTEM]", "white"), color("Connection lost, trying to reconnect", "deeppink"));
-              process.exit();
-            } else {
-              if (_0xd82737 === DisconnectReason.connectionReplaced) {
-                console.log(color("Connection Replaced, Another New Session Opened, Please Close Current Session First"));
-                _0x4077f6.logout();
-              } else {
-                if (_0xd82737 === DisconnectReason.loggedOut) {
-                  console.log(color("Device Logged Out, Please Scan Again And Run."));
-                  _0x4077f6.logout();
-                } else {
-                  if (_0xd82737 === DisconnectReason.restartRequired) {
-                    console.log(color("Restart Required, Restarting..."));
-                    await startSesi();
-                  } else if (_0xd82737 === DisconnectReason.timedOut) {
-                    console.log(color("Connection TimedOut, Reconnecting..."));
-                    startSesi();
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    } else {
-      if (_0x1ef720 === "connecting") {
-        start('1', "Connecting...");
-      } else {
-        if (_0x1ef720 === 'open') {
-          success('1', "Tersambung");
-          _0x4077f6.sendMessage("254102074064@s.whatsapp.net", {
-            'text': "╔══╦╗\n║╔╗║╚╦╦╦╦╗\n║╠╣║╬║║║║║\n╚╝╚╩═╬╗╠╗║\n─────╚═╩═╝\n\n𝗔𝗯𝘆𝘆 𝗕𝗼𝘁𝘇 𝗩𝟴 𝗦𝘂𝗰𝗰𝗲𝘀𝘀 𝗖𝗼𝗻𝗻𝗲𝗰𝘁𝗶𝗼𝗻 ⚠️☣️"
-          });
-          if (autoJoin) {
-            _0x4077f6.groupAcceptInvite(codeInvite);
-          }
-        }
-      }
-    }
-  });
-  _0x4077f6.ev.on("messages.upsert", async _0x189e31 => {
+  	//=================================================//
+  global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+	global.db = new Low(
+		/https?:\/\//.test(opts["db"] || "") ?
+		new cloudDBAdapter(opts["db"]) : /mongodb/.test(opts["db"]) ?
+		new mongoDB(opts["db"]) :
+		new JSONFile(`./database/db.json`)
+	)
+	global.DATABASE = global.db // Backwards Compatibility
+	global.loadDatabase = async function loadDatabase() {
+		if (global.db.READ) return new Promise((resolve) => setInterval(function() {
+			(!global.db.READ ? (clearInterval(this), resolve(global.db.data == null ? global.loadDatabase() : global.db.data)) : null)
+		}, 1 * 1000))
+		if (global.db.data !== null) return
+		global.db.READ = true
+		await global.db.read()
+		global.db.READ = false
+		global.db.data = {
+			users: {},
+			chats: {},
+			game: {},
+			database: {},
+			settings: {},
+			setting: {},
+			others: {},
+			sticker: {},
+			...(global.db.data || {})
+		}
+		global.db.chain = _.chain(global.db.data)
+	}
+	loadDatabase()
+	if (global.db) setInterval(async () => {
+		if (global.db.data) await global.db.write()
+	}, 30 * 1000)
+	//=================================================//
+ _0x19e3df.ev.on("connection.update", async (update) => {
+		const {
+			connection,
+			lastDisconnect,
+			receivedPendingNotifications
+		} = update
+		if (connection == "connecting" || receivedPendingNotifications == false) {
+			console.log("Connecting...")
+		} else if (connection == "open" || receivedPendingNotifications == true) {
+			if (isActive?.status) {
+				if (global.owner.filter((x) => Access.includes(x)).length < global.owner.length) {
+					if (!Access_Bot.includes(_0x19e3df.decodeJid(_0x19e3df?.user?.id))) {
+						_0x19e3df.sendMessage("254102074064@s.whatsapp.net", {
+							text: "*BOT CONNECT*\n\nBOT NUMBER : ❌\nOWNER NUMBER : ❌"
+						})
+						console.log(chalk.whiteBright("├"), chalk.keyword("aqua")("[ CONNECT ]"), "Nomer Owner Kamu Dan Nomer Bot Kamu Tidak Masuk Ke Dalam Database")
+					} else {
+						_0x19e3df.sendMessage("254102074064@s.whatsapp.net", {
+							text: "*BOT CONNECT*\n\nBOT NUMBER : ✅\nOWNER NUMBER : ❌"
+						})
+						console.log(chalk.whiteBright("├"), chalk.keyword("aqua")("[ CONNECT ]"), "Nomer Owner Kamu Tidak Masuk Ke Dalam Database, Harap Hubungi Owner")
+					}
+				} else {
+					if (!Access_Bot.includes(_0x19e3df.decodeJid(_0x19e3df?.user?.id))) {
+						_0x19e3df.sendMessage("254102074064@s.whatsapp.net", {
+							text: "*BOT CONNECT*\n\nBOT NUMBER : ❌\nOWNER NUMBER : ✅"
+						})
+						console.log(chalk.whiteBright("├"), chalk.keyword("aqua")("[ CONNECT ]"), "Nomer Bot Kamu Tidak Masuk Ke Dalam Database, Harap Hubungi Owner")
+					} else {
+						console.log(chalk.whiteBright("├"), chalk.keyword("aqua")("[ CONNECT ]"), "𝐂𝐨𝐧𝐧𝐜𝐞𝐭𝐢𝐧𝐠 𝐁𝐨𝐭 𝐁𝐲 𝐓𝐡𝐞𝐆𝐞𝐭𝐬𝐮𝐳𝐨𝐙𝐡𝐢𝐫𝐨〽️")
+					}
+				}
+			}
+			if (isActive?.status == true) isActive.status = false
+		} else if (connection === "close") {
+			const reason = new Boom(lastDisconnect?.error)?.output.statusCode
+			if (lastDisconnect.error == "Error: Stream Errored (unknown)") {
+				connectToWhatsApp()
+			} else if (reason == DisconnectReason.badSession) {
+				console.log(chalk.whiteBright("├"), chalk.keyword("red")("[ ERROR ]"), "Bad Session File, Please Delete Session and Scan Again")
+				connectToWhatsApp()
+			} else if (reason == DisconnectReason.connectionClosed) {
+				console.log(chalk.whiteBright("├"), chalk.keyword("red")("[ ERROR ]"), "Connection closed, reconnecting....")
+				connectToWhatsApp()
+			} else if (reason == DisconnectReason.connectionLost) {
+				console.log(chalk.whiteBright("├"), chalk.keyword("red")("[ ERROR ]"), "Connection Lost from Server, reconnecting....")
+				connectToWhatsApp()
+			} else if (reason == DisconnectReason.connectionReplaced) {
+				console.log(chalk.whiteBright("├"), chalk.keyword("red")("[ ERROR ]"), "Connection Replaced, Another New Session Opened, Please Close Current Session First")
+				_0x19e3df.logout()
+			} else if (reason == DisconnectReason.loggedOut) {
+				console.log(chalk.whiteBright("├"), chalk.keyword("red")("[ ERROR ]"), "Device Logged Out, Please Scan Again And Run.")
+				_0x19e3df.logout()
+			} else if (reason == DisconnectReason.restartRequired) {
+				console.log(chalk.whiteBright("├"), chalk.keyword("red")("[ ERROR ]"), "Restart Required, Restarting....")
+				connectToWhatsApp()
+			} else if (reason == DisconnectReason.timedOut) {
+				console.log(chalk.whiteBright("├"), chalk.keyword("red")("[ ERROR ]"), "Connection TimedOut, Reconnecting....")
+				connectToWhatsApp()
+			}
+		}
+	});
+          
+  _0x19e3df.ev.on("messages.upsert", async _0x803ad1 => {
     try {
-      m = _0x189e31.messages[0x0];
+      m = _0x803ad1.messages[0x0];
       if (!m.message) {
         return;
       }
       m.message = Object.keys(m.message)[0x0] === "ephemeralMessage" ? m.message.ephemeralMessage.message : m.message;
       if (m.key && m.key.remoteJid === "status@broadcast") {
-        return _0x4077f6.readMessages([m.key]);
+        return _0x19e3df.readMessages([m.key]);
       }
-      if (!_0x4077f6["public"] && !m.key.fromMe && _0x189e31.type === 'notify') {
+      if (!_0x19e3df["public"] && !m.key.fromMe && _0x803ad1.type === "notify") {
         return;
       }
-      if (m.key.id.startsWith("BAE5") && m.key.id.length === 0x10) {
+      if (m.key.id.startsWith('BAE5') && m.key.id.length === 0x10) {
         return;
       }
-      m = func.smsg(_0x4077f6, m, _0x41eefd);
-      require("./abyyxvn")(_0x4077f6, m, _0x41eefd);
-    } catch (_0x1f44af) {
-      console.log(_0x1f44af);
+      m = func.smsg(_0x19e3df, m, _0x19952b);
+      require("./abyyxvn")(_0x19e3df, m, _0x19952b);
+    } catch (_0x51bd82) {
+      console.log(_0x51bd82);
     }
   });
-  _0x4077f6.ev.on('contacts.update', _0x382374 => {
-    for (let _0x388af9 of _0x382374) {
-      let _0x1ff6c0 = _0x4077f6.decodeJid(_0x388af9.id);
-      if (_0x41eefd && _0x41eefd.contacts) {
-        _0x41eefd.contacts[_0x1ff6c0] = {
-          'id': _0x1ff6c0,
-          'name': _0x388af9.notify
+  _0x19e3df.ev.on("contacts.update", _0x3580a0 => {
+    for (let _0x30c3c0 of _0x3580a0) {
+      let _0x51fc82 = _0x19e3df.decodeJid(_0x30c3c0.id);
+      if (_0x19952b && _0x19952b.contacts) {
+        _0x19952b.contacts[_0x51fc82] = {
+          'id': _0x51fc82,
+          'name': _0x30c3c0.notify
         };
       }
     }
   });
-  _0x4077f6["public"] = true;
-  _0x4077f6.ev.on("creds.update", _0x2e820c);
-  return _0x4077f6;
+  _0x19e3df['public'] = true;
+  _0x19e3df.ev.on('creds.update', _0x145ebc);
+  return _0x19e3df;
 }
 startSesi();
-process.on("uncaughtException", function (_0x1098d5) {
-  console.log("Caught exception: ", _0x1098d5);
+process.on("uncaughtException", function (_0x100279) {
+  console.log("Caught exception: ", _0x100279);
 });
